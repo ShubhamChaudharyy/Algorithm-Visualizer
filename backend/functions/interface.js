9250285818c2d2e4d38dca082d4857b8ba631881
shubham <chd.nonu26@gmail.com>
2020-08-02 18:12:19 +0530

    update

--- backend/functions/interface.js
+++
@@ -1,5 +0,0 @@
-// import app from 'firebase/app'
-// export interface FirebaseClass{
-//     reDB: app.database.Database;
-//     bugreport:()=>app.database.Reference;
-// }
--- backend/functions/util/util.js
+++
@@ -1,7 +0,0 @@
-exports.isEmpty=(string)=>{
-    if(string.trim()==='') return true;
-    else return false;
-}
-exports.notMatching=(a)=>(b)=>{
-  return a.localeCompare(b); 
-}
