e1681c809626003d00413afb0b2b55fd8c4a56f5
shubham <chd.nonu26@gmail.com>
2020-08-04 20:13:15 +0530

    bubble sort implemented

--- src/components/startPage/AlgoView.tsx
+++ src/components/startPage/AlgoView.tsx
@@ -6,6 +6,7 @@ let i=0,j=0;
 export default()=>{
     const [chips,setChips]=useState<any[]>([])
     const [clicked,setClicked]=useState<any>(false)
+    const [tempArray,setTempArray]=useState<any[]>([])
     const [label,setLabel]=useState<any>('Visualise')
     const [done,setDone]=useState<any>(false)
     const [method,setMethod]=useState<any>('none')
@@ -14,26 +15,47 @@ export default()=>{
     'Quick Sort','Heap Sort','Bucket Sort',
     'Counting Sort','Radix Sort','Merge Sort'])
     const randomIntFromInterval=(min,max)=>Math.floor( Math.random()*  ( max - min + 1 ) + min )
-    const swap=(a,b)=>{
-        var h1=document.getElementById(`${a}`)
+    const swap=(x,a,b)=>{
+        setTimeout(()=>{
         //@ts-ignore
-        h1.style.backgroundColor='red';h1.style.height=`${chips[b]}px`
-        var h2=document.getElementById(`${b}`)
+        document.getElementById(`${a}`).style.backgroundColor="red";document.getElementById(`${b}`).style.backgroundColor="red";
+        setTimeout(()=>{
+        if(tempArray[a]>tempArray[b]){    
         //@ts-ignore
-        h2.style.backgroundColor='red';h2.style.height=`${chips[a]}px`
-
+        document.getElementById(`${a}`).style.height=`${chips[b]}px`;
+        //@ts-ignore
+        document.getElementById(`${b}`).style.height=`${chips[a]}px`;
+        let temp=tempArray[a];
+        tempArray[a]=tempArray[b];
+        tempArray[b]=temp;
+        }
+        setTimeout(()=>{
+        //@ts-ignore    
+        document.getElementById(`${a}`).style.backgroundColor=`#16f024`;document.getElementById(`${b}`).style.backgroundColor=`#16f024`
+        if(a==tempArray.length-2-x)
+        {j=0;i=x+1;}
+        else
+        {j=b;i=x;}
+        handleCheck();
+        },0)
+        },0)
+        },0)
+    }
+    const handleCheck=()=>{
+        bubble();
     }
     const bubble=()=>{
-        for(i=0;i<chips.length;i++){
-            for(j=0;j<chips.length-i-1;j++){
-                if(chips[j]>chips[j+1])
-                {swap(j,j+1);}
+        for(let m=i;m<tempArray.length;m++){
+            for(let k=j;k<tempArray.length-m-1;k++){
+                swap(m,k,k+1);
+                return;
             }
         }
     }
     const handleInput=async(e)=>{
         if(e.keyCode==13){
         setChips([...chips,e.target.value]) 
+        setTempArray([...chips,e.target.value])
         e.target.value=''
         }
         else
@@ -48,6 +70,7 @@ export default()=>{
                 console.log(method,"is performing!!!!!11")
                 setDone(true)
                 setLabel("Try Other Method")
+                i=0;j=0;
                 bubble();
             }
             else{
@@ -60,9 +83,10 @@ export default()=>{
     }
     const randomiseInput=()=>{
         var Array:any[]=[]
-        for(let i=0;i<10;i++)
+        for(let i=0;i<60;i++)
            Array.push(randomIntFromInterval(50,310))
-        setChips(Array)   
+        setChips(Array) 
+        setTempArray(Array)  
     }
     const handleSort=params=>(e)=>{
         setMethod(params)
